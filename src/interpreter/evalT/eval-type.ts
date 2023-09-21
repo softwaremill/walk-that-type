397ad50cad63649c1da8b08176c71b25240fddb2
Mieszko Sabo <51922326+mieszkosabo@users.noreply.github.com>
2023-09-21 21:49:39 +0200

    distributed unions (#2)

--- src/interpreter/evalT/eval-type.ts
+++ src/interpreter/evalT/eval-type.ts
@@ -4,11 +4,12 @@ import {
   Environment,
   extendEnvironment,
 } from "../environment";
-import { deepEquals, T, TypeNode } from "../type-node";
+import { deepEquals, T, traverse, TypeNode } from "../type-node";
 import { Do, err, ok, Result } from "this-is-ok/result";
 import { sequence } from "../type-node/map-AST-to-type-nodes";
 import { extendsT } from "../extendsT/extendsT";
 import { intrinsicTypes } from "./intrinsic-types";
+import { cartesianProduct } from "../../utils/cartesianProduct";
 
 const removeDuplicates = (arr: TypeNode[]): TypeNode[] => {
   const uniq: TypeNode[] = [];
@@ -100,9 +101,9 @@ export const evalT = (
       });
     })
     .with({ _type: "typeReference" }, (t) => {
-      const globalTypeImpl = intrinsicTypes[t.name];
-      if (globalTypeImpl) {
-        return globalTypeImpl.fn(t.typeArguments).flatMap((type) => {
+      const intrinsicTypeImpl = intrinsicTypes[t.name];
+      if (intrinsicTypeImpl) {
+        return intrinsicTypeImpl.fn(t.typeArguments).flatMap((type) => {
           return evalT(env, type);
         });
       }
@@ -111,6 +112,62 @@ export const evalT = (
       if (!typeDeclaration) {
         return err(new Error(`unknown type ${t.name}`));
       }
+
+      // This part below is for handling distributed unions.
+      // For example:
+      // type ToArray<Type> = Type extends any ? Type[] : never;
+      //                      ^ typeDeclaration.type
+      // ToArray<string | number>
+      //         ^ t.typeArguments[0]
+      // This should evaluate to -> ToArray<string> | ToArray<number>
+      // So string[] | number[] and not (string | number)[]
+      // A generic type distributes over its arguments if:
+      // 1. the argument is a union type
+      // 2. It's used on the left-hand side of a conditional type as a ***naked type***.
+      // The conditional type could be nested arbitrarily deep inside the type.
+
+      // Indices of t.typeArguments that are unions and need to be distributed.
+      // To calculate that, we need to traverse `typeDeclaration.type` and check if
+      // there is a conditional type with naked t.typeArgument (we do it for each t.typeArguments).
+      const indicesOfUnionsToDistribute = typeDeclaration.typeParameters.reduce(
+        (acc, val, idx) => {
+          if (t.typeArguments[idx]?._type !== "union") {
+            return acc;
+          }
+          traverse(typeDeclaration.type, (tt) => {
+            if (
+              tt._type === "conditionalType" &&
+              tt.checkType._type === "typeReference" &&
+              tt.checkType.name === val
+            ) {
+              if (!acc.includes(idx)) {
+                acc.push(idx);
+              }
+            }
+          });
+
+          return acc;
+        },
+        [] as number[]
+      );
+
+      if (indicesOfUnionsToDistribute.length > 0) {
+        // Map t.typeArguments so that distributed unions are mapped to nested arrays.
+        // The rest is transformed into a singleton.
+        const args = t.typeArguments.map((tt, idx) =>
+          tt._type === "union" && indicesOfUnionsToDistribute.includes(idx)
+            ? tt.members
+            : [tt]
+        );
+
+        const argsSets = cartesianProduct(...args);
+        // return union of t.name (type reference) with each set of arguments from cartesian product
+        return evalT(
+          env,
+          T.union(argsSets.map((set) => T.typeReference(t.name, set)))
+        );
+      }
+      // end of distributed unions
 
       const newEnv = { ...env };
       typeDeclaration.typeParameters.forEach((ty, idx) => {
